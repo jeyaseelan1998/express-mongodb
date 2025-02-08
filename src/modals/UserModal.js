9d512798767ec58a8c7c29a858503ba83b2b59bf
jeyaseelan <jeyaseelan.work@gmail.com>
2025-02-08 14:37:32 +0530

    Add login, registration

--- src/modals/UserModal.js
+++ src/modals/UserModal.js
@@ -26,41 +26,6 @@ const userSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-    if (this.modified("password")) {
-        this.password = bcrypt.hash(this.password, 10);
-    }
-    next();
-});
-
-userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.generateAccessToken = function () {
-    jwt.sign(
-        {
-            _id: this._id,
-            username: this.username,
-            password: this.password,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-    );
-};
-
-userSchema.methods.generateRefreshToken = function () {
-    jwt.sign(
-        {
-            _id: this._id,
-            username: this.username,
-            password: this.password,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
-    );
-};
-
 userSchema.plugin(mongooseAggregate);
 
 module.exports = mongoose.model("User", userSchema);
